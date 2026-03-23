f1278f65fb03f7b398b801bd4c28df7e0f94c6fe
Capilot989 <capitan22890@gmail.com>
2026-03-23 17:33:36 +0700

    Create solution.py

---
+++ ex_3/solution.py
@@ -0,0 +1,32 @@
+
+
+class NotSleeping:
+    """
+      A class representing a person who cannot fall asleep and counts sheep.
+
+      This class tracks the number of sheep counted as a method to help
+      the person fall asleep.
+
+      Attributes:
+          name: The name of the person who can't sleep
+          count_sheeps: The number of sheep counted so far
+    """
+    def __init__(self, name: str, count: int=0) -> None:
+        """
+            Initialize a new NotSleeping instance.
+
+            Args:
+                name: The name of the person who can't sleep
+                count: Initial number of sheep counted (default: 0)
+        """
+        self.name = name
+        self.count_sheeps = count
+
+    def add_sheep(self) -> None:
+        """
+               Increment the sheep count by one.
+
+               This method represents counting one additional sheep in an attempt
+               to fall asleep.
+        """
+        self.count_sheeps += 1
