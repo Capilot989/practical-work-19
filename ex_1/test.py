b6b08d27c42dc79553d9daffc43375d9b8130167
Capilot989 <capitan22890@gmail.com>
2026-03-23 17:13:56 +0700

    Update test.py

--- ex_1/test.py
+++ ex_1/test.py
@@ -1,7 +1,8 @@
 from solution import Dog
 
 
-small_dog = Dog('Снупи')
-print(small_dog.name)
-small_dog.say()
-print('Кличка собаки', small_dog.name)
+if __name__ == "__main__":
+    small_dog = Dog('Снупи')
+    print(small_dog.name)
+    small_dog.say()
+    print('Кличка собаки', small_dog.name)
