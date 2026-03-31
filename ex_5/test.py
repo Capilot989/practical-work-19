b4a5bc5bede518552a5d67ba58d51566d5e85139
Capilot989 <capitan22890@gmail.com>
2026-03-31 09:14:52 +0700

    Create test.py

---
+++ ex_5/test.py
@@ -0,0 +1,15 @@
+from solution import Game
+
+
+if __name__ == "__main__":
+    game_one = Game({'command1': 'Юта Джаз', 'command2': 'Майами Хит'})
+    game_one.ball_thrown(1, 2)
+    game_one.ball_thrown(1, 3)
+    game_one.ball_thrown(2, 2)
+    game_one.ball_thrown(1, 1)
+    print(game_one.get_score())
+    game_one.ball_thrown(2, 3)
+    game_one.ball_thrown(2, 2)
+    game_one.ball_thrown(1, 2)
+    print(game_one.get_score())
+    print(game_one.get_winner())
