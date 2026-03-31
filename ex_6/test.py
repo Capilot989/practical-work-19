d2ed8f6638dede1f0d64af54b7200b143b7fddc9
Capilot989 <capitan22890@gmail.com>
2026-03-31 10:14:33 +0700

    Create test.py

---
+++ ex_6/test.py
@@ -0,0 +1,14 @@
+from solution import Point
+
+
+if __name__ == "__main__":
+    p = Point((3, -7))
+    print(p)
+    a = Point()
+    print(a)
+    print(a.get_x())
+    print(p.get_y())
+    c = Point((-2, 4))
+    print(p.distance(c))
+    d = c.sum(p)
+    print(d)
