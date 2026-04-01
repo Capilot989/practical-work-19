b64a6d09a20ff0a6a6db3a29aff63698662f3f3f
Capilot989 <capitan22890@gmail.com>
2026-04-01 15:53:28 +0700

    Create test.py

---
+++ ex_9/test.py
@@ -0,0 +1,11 @@
+from solution import StrandsDNA
+
+
+if __name__ == "__main__":
+    covid_19 = StrandsDNA()
+    covid_19.add_strands('GAAT ACCGTT TTGAC TGGGAC')
+    print(covid_19)
+    covid_19.add_strands('ACCT AGGCT TGGGAC')
+    covid_19.add_strands('CATTTT TAATTC')
+    print(covid_19)
+    print(covid_19.get_max_strands())
